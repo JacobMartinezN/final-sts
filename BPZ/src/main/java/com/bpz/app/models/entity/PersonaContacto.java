ad6f5ea839ecd3b7e3054a18aabdd3ad75c7b211
Jacob Martinez <m.a.r.t.i.n.e.z.2.2470@gmail.com>
2018-05-25 22:40:42 -0500

    persona contacto entity OK

--- BPZ/src/main/java/com/bpz/app/models/entity/PersonaContacto.java
+++ BPZ/src/main/java/com/bpz/app/models/entity/PersonaContacto.java
@@ -1,5 +1,33 @@
 package com.bpz.app.models.entity;
 
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "Persona_Contacto")
 public class PersonaContacto {
 
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	private String nombre;
+	
+	@NotNull
+	private  String apellido;
+	
+	@NotNull
+	private Long dni;
+	
+	@NotNull
+	private Long celular;
+	
+	@NotNull
+	@Email
+	private String correo;
+	
 }
