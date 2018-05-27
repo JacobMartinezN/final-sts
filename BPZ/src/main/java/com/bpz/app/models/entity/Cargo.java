e47ac244c676ef23f467b79c41a18374e267a265
Francesca <Francesca@192.168.1.25>
2018-05-26 19:21:51 -0500

    Entity_3

--- BPZ/src/main/java/com/bpz/app/models/entity/Cargo.java
+++ BPZ/src/main/java/com/bpz/app/models/entity/Cargo.java
@@ -1,5 +1,28 @@
 package com.bpz.app.models.entity;
 
-public class Cargo {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "Cargo")
+public class Cargo implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idCargo;
+	
+	@NotNull
+	private String nombre;
+	
+	@NotNull
+	private double sueldo;
+	
 }
